7eac98099da2439022d9bbb570f5537f479e9bee
noor-khan <foruconnect@gmail.com>
2020-06-01 20:44:08 +0500

    add navbar, scss, splash page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,66 @@
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-    @select="handleSelect"
-  >
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-submenu index="2">
-      <template slot="title">Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-submenu index="2-4">
-        <template slot="title">item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">
-      <a href="https://www.ele.me" target="_blank">Orders</a>
-    </el-menu-item>
-  </el-menu>
+  <div id="navbar">
+    <TopNav />
+    <div
+      class="bottom-nav"
+      :style="{ top: scrollPoint > 30 ? scrollPoint - scrollPoint : '' }"
+      style="transition: 1s"
+    >
+      <div class="nav-brand">
+        <h2><span style="color:#ee2625;">spicy</span>mandarin</h2>
+      </div>
+      <el-menu class="el-menu-demo" mode="horizontal">
+        <el-menu-item index="1">Home</el-menu-item>
+        <el-menu-item index="2">About</el-menu-item>
+        <el-menu-item index="4">Our Talents</el-menu-item>
+        <el-menu-item index="4">Contact Us</el-menu-item>
+        <!-- <span>{{ scrollPoint }}</span> -->
+      </el-menu>
+    </div>
+  </div>
 </template>
 <script>
-export default {};
+import TopNav from "@/components/TopNav.vue";
+export default {
+  props: ["scrollPoint"],
+  components: {
+    TopNav
+  }
+};
 </script>
+<style lang="scss">
+#navbar {
+  .bottom-nav {
+    position: fixed;
+    width: 100%;
+    // padding: 0 40px;
+    .nav-brand {
+      padding-left: 40px;
+      text-align: left;
+      float: left;
+      h2 {
+        font-weight: 100;
+        color: #f6f6f6;
+        text-transform: uppercase;
+      }
+    }
+    .el-menu {
+      float: right;
+      border: none;
+      background: none;
+      padding-right: 40px;
+      .el-menu-item {
+        font-size: 14px;
+        color: #f6f6f6;
+        text-transform: uppercase;
+        font-weight: 300;
+        &:hover {
+          background: none;
+          color: #ee2625;
+          font-weight: 400;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/home/SplashVideo.vue
@@ -0,0 +1,57 @@
+<template>
+  <section id="SplashVideo">
+    <el-row>
+      <el-col :span="24">
+        <div class="content-wrapper">
+          <div class="main-content">
+            <h2><span style="color:#ee2625">spicy</span>mandarin</h2>
+            <h4>unconventional way or method of teaching Mandarin</h4>
+            <DefaultButton text="Let's get started" />
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+  </section>
+</template>
+<script>
+import DefaultButton from "../buttons/Default";
+export default {
+  components: {
+    DefaultButton
+  }
+};
+</script>
+<style lang="scss">
+#SplashVideo {
+  clear: both;
+  height: 100vh;
+  background-image: url("../../assets/images/videoimg.jpg");
+  .content-wrapper {
+    display: table;
+    width: 100%;
+    height: 100vh;
+    .main-content {
+      display: table-cell;
+      vertical-align: middle;
+      text-transform: uppercase;
+      h2 {
+        font-size: 48px;
+        font-family: "Rosarivo";
+        letter-spacing: 16px;
+        font-weight: 400;
+        margin-bottom: 0;
+      }
+      h4 {
+        margin-top: 0;
+        font-size: 14px;
+        color: #fff;
+        font-weight: 100;
+        letter-spacing: 4.5px;
+      }
+      button {
+        margin-top: 40px;
+      }
+    }
+  }
+}
+</style>
